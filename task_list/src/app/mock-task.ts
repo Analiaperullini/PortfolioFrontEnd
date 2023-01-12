9f1aa8fa6088658edc84939cc40a444801619776
Analia <analiaperullini@gmail.com>
2023-01-12 20:02:36 -0300

    Agregar mock de tareas y el interface de las mismas

---
+++ task_list/src/app/mock-task.ts
@@ -0,0 +1,26 @@
+export const TASKS = [
+{
+    id: 1,
+    text: "Terminar primer modulo de Angular",
+    day: 'Enero 12 a las 20:00',
+    reminder: true
+},
+{
+    id: 2,
+    text: "Hacer las compras para la cena",
+    day: 'Enero 12 a las 22:00',
+    reminder: true
+},
+{
+    id: 3,
+    text: "Investigar sobre Boostrap",
+    day: 'Enero 13 a las 18:00',
+    reminder: false
+},
+{
+    id: 4,
+    text: "Jugar con los perros",
+    day: 'Enero 13 a las 19:00',
+    reminder: false
+},
+]
